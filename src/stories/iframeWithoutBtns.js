b24dc27ce779cb2155c162c7a3506322b348e8d5
abanik <arnab_banik@intuit.com>
2021-09-08 08:52:22 -0700

    beautify non-oauth for partners

--- src/stories/iframeWithoutBtns.js
+++ src/stories/iframeWithoutBtns.js
@@ -171,7 +171,7 @@ export const IframeWithIntuitButtons = ({ theme, isAggregatorScreenFirstInWidget
 						title={"my awesome iframe"}
 						onLoad={handleIframeOnLoad}
 						ref={iframeRef}
-						src={`http://localhost:3000?theme=${theme}&isAggregatorScreenFirstInWidgets=${isAggregatorScreenFirstInWidgets}&shouldDisplayIntuitFooter=true`}
+						src={`https://non-oauth-sage.vercel.app/?theme=${theme}&isAggregatorScreenFirstInWidgets=${isAggregatorScreenFirstInWidgets}&shouldDisplayIntuitFooter=true`}
 						frameBorder="0"
 						scrolling="no"
 					/>
